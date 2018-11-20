4f2c29567db166854ad4f0c48ae34248be963fea
mikel lopez <mikelsalazarlopez@gmail.com>
2018-11-20 17:45:40 +0100

    custom location changes

--- app/src/main/java/com/carpark/mls/carparking/AppConfig/CustomLocation.java
+++ app/src/main/java/com/carpark/mls/carparking/AppConfig/CustomLocation.java
@@ -3,55 +3,49 @@ package com.carpark.mls.carparking.AppConfig;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 
-import com.carpark.mls.carparking.Interfaces.DialogInterface;
 import com.carpark.mls.carparking.Interfaces.LocationInterface;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 public class CustomLocation {
 
-    private FusedLocationProviderClient mFusedLocationClient;
     private Context context;
-
+    private LocationManager locationManager;
 
 
     public CustomLocation(Context context){
         this.context = context;
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.context);
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
     }
 
-    private static LocationRequest createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(200000);
-        mLocationRequest.setFastestInterval(300000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return mLocationRequest;
-    }
+
 
     public void getActualLocation(){
 
-        if ( ContextCompat.checkSelfPermission( context,
+        final LocationInterface interfazEspera = (LocationInterface) context;
+
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                interfazEspera.localizacion(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+        if (ContextCompat.checkSelfPermission( context,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
 
-            final LocationInterface interfazEspera = (LocationInterface) context;
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    // Got last known location. In some rare situations this can be null.
-                    if (location != null) {
-                        interfazEspera.localizacion(location);
-                    }else{
-                        Utils.showToast(context,"Location null");
-                    }
-                }
-            });
-        }else{
-            Utils.showToast(context,"No hay permisos");
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         }
+
 
     }
 
